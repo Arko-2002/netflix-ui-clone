68a0d6d7cfd889886d82b7a630f31f6938a688f1
Arko-2002 <89196817+Arko-2002@users.noreply.github.com>
2023-07-31 12:55:54 +0530

    Add files via upload

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,35 @@
+import "./Login.scss";
+import { Link } from "react-router-dom";
+
+export default function Login() {
+  return (
+    <div className="login">
+      <div className="Ttop">
+        <div className="Wwrapper">
+          <img
+            className="bglogo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="Tcontainer">
+        <form>
+          <h1>Sign In</h1>
+          <input type="email" placeholder="Email or Phone Number" />
+          <input type="password" placeholder="Password" />
+          <button className="log">
+            <Link to="/home">Sign In</Link>
+          </button>
+          <span>
+            New to Netflix ? <b>Sign up now.</b>
+          </span>
+          <small>
+            This page is protected by Google reCAPTCHA to ensure youre not a
+            bot. <b>Learn more</b>.
+          </small>
+        </form>
+      </div>
+    </div>
+  );
+}
